1d47fda16699c95e26d6b8248a951ba5bf72d002
Ivan Pfefferkorn <ivanpfeff@gmail.com>
2021-08-14 15:02:07 -0400

    Adding autofac wireup for IoC

---
+++ SeoSampleApp/SeoSampleApp/IoC/SearchModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeoSampleApp.IoC
+{
+    public class SearchModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            //Register search related components
+        }
+    }
+}
--- SeoSampleApp/SeoSampleApp/Startup.cs
+++ SeoSampleApp/SeoSampleApp/Startup.cs
@@ -1,9 +1,11 @@
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SeoSampleApp.IoC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,11 @@ namespace SeoSampleApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule<SearchModule>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
