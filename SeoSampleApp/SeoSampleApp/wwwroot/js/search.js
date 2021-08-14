9e2b3623983c2f8fbd16a05b0634e0efbd368443
Ivan Pfefferkorn <ivanpfeff@gmail.com>
2021-08-14 14:57:28 -0400

    Initial commit, it's just an ASP.NET core sample skeleton app but with a slightly modified index page

---
+++ SeoSampleApp/SeoSampleApp/wwwroot/js/search.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    var ViewModel = function () {
+        var self = this;
+
+        this.useGoogle = ko.observable(true);
+        this.searchURL = ko.observable("");
+        this.searchTerm = ko.observable("");
+        this.seoTerm = ko.observable("");
+
+        this.searchesLoading = ko.observable(true);
+        this.previousSearches = ko.observableArray([]);
+    }
+
+    var vm = new ViewModel();
+    ko.applyBindings(vm);
+});
